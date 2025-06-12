b965508fcbe2e149f5cd454d1a6a66004a094c5b
FirstFlush <firstflush@protonmail.com>
2025-06-11 21:50:12 -0700

    WIP building dir structure

---
+++ src/enums/scraping.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+# class InputType(Enum):
+    
+#     HTML = "html"
+#     PDF = "pdf"
+    
---
+++ src/enums/spider_keys.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class SpiderKeys(Enum):
+    
+    BC211_HTML = "bc211.html.search"
+    BC211_PDF = "bc211.pdf.shelters"
